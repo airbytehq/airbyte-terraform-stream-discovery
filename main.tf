7656d81b1ad1d74691e9fe9a8b933cd7143b6d69
taanielj <taaniel.jakobson@gmail.com>
2024-02-01 12:44:17 +0200

    added support for basic auth

--- main.tf
+++ main.tf
@@ -8,6 +8,14 @@ terraform {
   }
 }
 
+locals {
+  authorization_header = (
+    var.airbyte_api_token != "" ?
+    "Bearer ${var.airbyte_api_token}" :
+    "Basic ${base64encode("${var.airbyte_basic_auth_username}:${var.airbyte_basic_auth_password}")}"
+    )
+}
+
 data "http" "stream_source" {
   url = "${var.airbyte_host_url}/v1/streams?sourceId=${var.source_id}&destinationId=${var.destination_id}&ignoreCache=${var.ignore_cache}"
 
--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "airbyte_api_token" {
   type = string
   sensitive = true
   description = "A valid api token for airbyte"
+  validation {
+    condition = var.airbyte_api_token != "" || (var.airbyte_basic_auth_username != "" && var.airbyte_basic_auth_password != "")
+    error_message = "Either an api token or basic auth credentials must be provided"
+  }
 }
 
 variable "source_id" {
@@ -18,6 +22,17 @@ variable "airbyte_host_url" {
   type = string
   description = "URL where airbyte is hosted"
   default = "https://api.airbyte.com"
+}
+
+variable airbyte_basic_auth_username {
+  type = string
+  description = "Username for basic auth"
+}
+
+variable airbyte_basic_auth_password {
+  type = string
+  description = "Password for basic auth"
+  sensitive = true
 }
 
 variable "ignore_cache" {
